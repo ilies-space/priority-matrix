2e05b42ffbf49f9d3877fd166a32149f5fcf0efd
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-02-23 16:10:14 +0100

    init variables

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
 
 export default function App() {
+  // variables  :
+  const [ImportantAndUrgent, setImportantAndUrgent] = useState([]);
+  const [ImportantButNotUrgent, setImportantButNotUrgent] = useState([]);
+  const [NotImportantButUrgent, setNotImportantButUrgent] = useState([]);
+  const [NotImportantAndNotUrgent, setNotImportantAndNotUrgent] = useState([]);
   return (
     <View>
       <Text>HOME</Text>
