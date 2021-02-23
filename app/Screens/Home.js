571358c5418932cb729bc9266c6c2ffff374e441
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-02-23 16:41:55 +0100

    best practice app folder structure

---
+++ app/Screens/Home.js
@@ -0,0 +1,152 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Button,
+  Alert,
+} from 'react-native';
+
+export default function Home() {
+  // variables  :
+  const [ImportantAndUrgent, setImportantAndUrgent] = useState([
+    {
+      title: 'task exmple',
+      description:
+        'Minim laborum esse anim nostrud deserunt eiusmod mollit magna non occaecat eu deserunt elit.',
+    },
+    {
+      title: 'task exmple2',
+      description:
+        '2 Minim laborum esse anim nostrud deserunt eiusmod mollit magna non occaecat eu deserunt elit.',
+    },
+  ]);
+  const [ImportantButNotUrgent, setImportantButNotUrgent] = useState([]);
+  const [NotImportantButUrgent, setNotImportantButUrgent] = useState([]);
+  const [NotImportantAndNotUrgent, setNotImportantAndNotUrgent] = useState([]);
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'gray',
+      }}>
+      {/* TOP -  */}
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'red',
+          flexDirection: 'row',
+        }}>
+        {/* 1 : ImportantAndUrgent */}
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'pink',
+          }}>
+          {/* Title  */}
+          <View
+            style={{
+              backgroundColor: 'white',
+              flexDirection: 'row',
+              justifyContent: 'space-around',
+              alignItems: 'center',
+            }}>
+            <Text
+              style={{
+                padding: 2,
+              }}>
+              Important and urgent
+            </Text>
+            <TouchableOpacity
+              onPress={() => {
+                console.log('add new element');
+                setImportantAndUrgent((prevList) => {
+                  return [
+                    {
+                      title: 'Task from user',
+                      description:
+                        'Occaecat ea tempor consequat magna nulla quis aliquip proident tempor do laborum deserunt.',
+                    },
+                    ...prevList,
+                  ];
+                });
+              }}>
+              <Text
+                style={{
+                  backgroundColor: 'green',
+                  height: 20,
+                  width: 20,
+                  borderRadius: 10,
+                  textAlign: 'center',
+                  color: 'white',
+                }}>
+                +
+              </Text>
+            </TouchableOpacity>
+          </View>
+          {/* List of items  */}
+          <FlatList
+            data={ImportantAndUrgent}
+            keyExtractor={(item, index) => index}
+            renderItem={({item}) => {
+              return (
+                <TouchableOpacity
+                  onPress={() => {
+                    Alert.alert(item.title, item.description, [
+                      {
+                        text: 'cancel',
+                      },
+                      {
+                        text: 'delete',
+                        onPress: () => {
+                          setImportantAndUrgent((prevList) => {
+                            return prevList.filter((elm) => elm != item);
+                          });
+                        },
+                      },
+                    ]);
+                  }}
+                  style={{
+                    padding: 5,
+                    marginVertical: 2,
+                  }}>
+                  <Text> {item.title} </Text>
+                </TouchableOpacity>
+              );
+            }}
+          />
+        </View>
+
+        {/* 2 */}
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'black',
+          }}></View>
+      </View>
+
+      {/* Bottom -  */}
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'red',
+          flexDirection: 'row',
+        }}>
+        {/* 1 */}
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'red',
+          }}></View>
+
+        {/* 2 */}
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'yellow',
+          }}></View>
+      </View>
+    </View>
+  );
+}
