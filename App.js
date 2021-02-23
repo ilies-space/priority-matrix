70500e3a71312a894bf3b193f128bd71a16bed65
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-02-23 19:28:01 +0100

    connect store to app js

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Home from './app/Screens/Home';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/es/integration/react';
+import store, {peristedStore} from './app/Redux/store';
 
 export default function App() {
   return <Home />;
