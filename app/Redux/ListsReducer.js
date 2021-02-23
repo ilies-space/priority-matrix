ec8fc26e464045fca05c751019055a015a538fae
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-02-23 20:35:06 +0100

    reducer for ImportantButNotUrgent

--- app/Redux/ListsReducer.js
+++ app/Redux/ListsReducer.js
@@ -1,6 +1,10 @@
 export const AddToImportantAndUrgent = 'AddToImportantAndUrgent';
 export const DeleteFromImportantAndUrgent = 'DeleteFromImportantAndUrgent';
 
+export const AddToImportantButNotUrgent = 'AddToImportantButNotUrgent';
+export const DeleteFromImportantButNotUrgent =
+  'DeleteFromImportantButNotUrgent';
+
 const initialState = {
   ImportantAndUrgent: [],
   ImportantButNotUrgent: [],
@@ -10,7 +14,7 @@ const initialState = {
 
 const ListsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // ----------
+    //ImportantAndUrgent
 
     case AddToImportantAndUrgent:
       console.log(AddToImportantAndUrgent);
@@ -24,6 +28,27 @@ const ListsReducer = (state = initialState, action) => {
       state.ImportantAndUrgent = state.ImportantAndUrgent.filter((element) => {
         return element !== action.selectedItem;
       });
+      return state;
+
+    //ImportantButNotUrgent
+
+    case AddToImportantButNotUrgent:
+      console.log(AddToImportantButNotUrgent);
+      state.ImportantButNotUrgent = [
+        action.newItem,
+        ...state.ImportantButNotUrgent,
+      ];
+      return state;
+
+    // ----------
+
+    case DeleteFromImportantButNotUrgent:
+      console.log(DeleteFromImportantButNotUrgent);
+      state.ImportantButNotUrgent = state.ImportantButNotUrgent.filter(
+        (element) => {
+          return element !== action.selectedItem;
+        },
+      );
       return state;
 
     default:
