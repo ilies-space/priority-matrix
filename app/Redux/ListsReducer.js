ee2d2f4e6fc7adf3ec5f90f7d8ed5c51f2d54a6b
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-02-23 19:41:35 +0100

    beta ListsReducer

--- app/Redux/ListsReducer.js
+++ app/Redux/ListsReducer.js
@@ -1,83 +1,29 @@
-export const changeTodoItemState = 'changeTodoItemState';
-export const AddTodoItem = 'AddTodoItem';
-export const DeleteTodoItem = 'DeleteTodoItem';
-export const ArchiveTodoItem = 'ArchiveTodoItem';
-export const ReturnArchiveTodoItem = 'ReturnArchiveTodoItem';
-export const UpdatePassword = 'UpdatePassword';
+export const AddToImportantAndUrgent = 'AddToImportantAndUrgent';
+export const DeleteFromImportantAndUrgent = 'DeleteFromImportantAndUrgent';
 
 const initialState = {
-  toDoList: [],
-  toDoListArchived: [],
-  ArchivedListPassword: '0000',
+  ImportantAndUrgent: [],
+  ImportantButNotUrgent: [],
+  NotImportantButUrgent: [],
+  NotImportantAndNotUrgent: [],
 };
 
 const ListsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case changeTodoItemState:
-      state.toDoList.forEach((elm) => {
-        if (elm.id === action.selectedTodoItem.id) {
-          elm.isDone = !elm.isDone;
-        }
+    // ----------
+
+    case AddToImportantAndUrgent:
+      console.log(AddToImportantAndUrgent);
+      state.ImportantAndUrgent = [action.newItem, ...state.ImportantAndUrgent];
+      return state;
+
+    // ----------
+
+    case DeleteFromImportantAndUrgent:
+      state.ImportantAndUrgent = state.ImportantAndUrgent.filter((element) => {
+        return element !== action.selectedItem;
       });
-      return {
-        toDoList: state.toDoList,
-        toDoListArchived: state.toDoListArchived,
-        ArchivedListPassword: state.ArchivedListPassword,
-      };
-
-    case AddTodoItem:
-      state.toDoList = [action.newTodoItem, ...state.toDoList];
-      return {
-        toDoList: state.toDoList,
-        toDoListArchived: state.toDoListArchived,
-        ArchivedListPassword: state.ArchivedListPassword,
-      };
-
-    case DeleteTodoItem:
-      state.toDoList = state.toDoList.filter((todoItem) => {
-        return todoItem !== action.selectedTodoItem;
-      });
-      return {
-        toDoList: state.toDoList,
-        toDoListArchived: state.toDoListArchived,
-        ArchivedListPassword: state.ArchivedListPassword,
-      };
-
-    case ArchiveTodoItem:
-      state.toDoList = state.toDoList.filter((todoItem) => {
-        return todoItem !== action.selectedTodoItem;
-      });
-
-      state.toDoListArchived = [
-        action.selectedTodoItem,
-        ...state.toDoListArchived,
-      ];
-      return {
-        toDoList: state.toDoList,
-        toDoListArchived: state.toDoListArchived,
-        ArchivedListPassword: state.ArchivedListPassword,
-      };
-
-    case ReturnArchiveTodoItem:
-      state.toDoListArchived = state.toDoListArchived.filter((todoItem) => {
-        return todoItem !== action.selectedTodoItem;
-      });
-
-      state.toDoList = [action.selectedTodoItem, ...state.toDoList];
-      return {
-        toDoList: state.toDoList,
-        toDoListArchived: state.toDoListArchived,
-        ArchivedListPassword: state.ArchivedListPassword,
-      };
-
-    case UpdatePassword:
-      state.ArchivedListPassword = action.newPassword;
-
-      return {
-        toDoList: state.toDoList,
-        toDoListArchived: state.toDoListArchived,
-        ArchivedListPassword: state.ArchivedListPassword,
-      };
+      return state;
 
     default:
       return state;
